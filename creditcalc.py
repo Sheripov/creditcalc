c91900a5a3ed87dac34861eb52aecf22017c1989
Farruh Sheripov <sheripov.farruh@gmail.com>
2020-07-04 20:00:35 +0300

    calc month/payment

--- creditcalc.py
+++ creditcalc.py
@@ -1,11 +1,25 @@
-credit_principal = 'Credit principal: 1000'
-final_output = 'The credit has been repaid!'
-first_month = 'Month 1: paid out 250'
-second_month = 'Month 2: paid out 250'
-third_month = 'Month 3: paid out 500'
+from math import ceil
+print("Enter the credit principal:")
+credit_principal = int(input())
 
-print(credit_principal)
-print(first_month)
-print(second_month)
-print(third_month)
-print(final_output)
+print('What do you want to calculate?')
+print('type "m" - for count of months,')
+print('type "p" - for monthly payment:')
+choose = input()
+if choose.lower() == "m":
+    print('Enter monthly payment:')
+    monthly_payment = int(input())
+    months_to_repay = credit_principal / monthly_payment
+    if months_to_repay <= 1:
+        print(f'It takes 1 month to repay the credit')
+    else:
+        print(f'It takes {round(months_to_repay)} months to repay the credit')
+elif choose.lower() == "p":
+    print('Enter count of months:')
+    count_of_month = int(input())
+    monthly_payment = credit_principal / count_of_month
+    if monthly_payment % 1 == 0:
+        print(f'Your monthly payment = {int(monthly_payment)}')
+    else:
+        last_payment = credit_principal - (count_of_month - 1) * ceil(monthly_payment)
+        print(f'Your monthly payment = {ceil(monthly_payment)} with last month payment = {last_payment}')
